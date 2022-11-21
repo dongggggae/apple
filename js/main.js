7720f2fbaa9a47a5c64d4bd837a561f2501a9df2
김동현 <wjdqls149@naver.com>
2022-11-21 22:25:08 +0900

    오늘은 여기까지

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function () {
     }
     beforeScr = afterScr;
   });
-
+  // 마우스 움직이면서 x좌표 바꾸기
   $(".main-visual").mousemove(function (e) {
     sectWidth = $(this).outerWidth() / 2;
     xPos = (e.clientX - sectWidth) / 6;
@@ -17,4 +17,30 @@ $(function () {
       x: xPos,
     });
   });
+  // gsap scroll
+  const watchIntro = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".watch-intro",
+      markers: true,
+      start: "top top",
+      end: "+=550%",
+      scrub: 5,
+      pin: true,
+    },
+  });
+
+  watchIntro
+    .addLabel("a")
+    .to(".watch-intro .play span", { yPercent: -100 }, "a")
+    .to(".watch-intro .replay span", { yPercent: 100 }, "a")
+    .to(".watch-intro .text-main p", { opacity: 1, stagger: 0.5 })
+    .to(".watch-intro .text-main p", { opacity: 0 })
+    .to(".watch-intro .text-sub", { visibility: "visible", opacity: 1 })
+    .to(".watch-intro .text-sub span", {
+      opacity: 1,
+      yPercent: -100,
+      stagger: 0.2,
+    })
+    .to(".watch-intro .text-sub span", { opacity: 0.1 });
 });
+// git push -u origin main
